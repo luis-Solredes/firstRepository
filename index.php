a89a2dc2e1b17ce7d2448cf29b97471c3b805268
luis-Solredes <luis.gualteros@solredes.biz>
2015-01-09 14:49:30 -0500

    probar servidor
    prueba php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Hola php</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<?php 
+	$text="Hola mundo";
+	echo $text;
+	?>
+</body>
+</html><!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Hola php</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<?php 
+	$text="Hola mundo";
+	echo $text;
+	?>
+</body>
+</html>
